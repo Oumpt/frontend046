ebc4977725a59484e4ec18578fd79191ce520f36
Oumpt <pongthepuon@gmail.com>
2025-07-07 12:48:11 +0700

    Update Footer.js

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -126,7 +126,7 @@ export default function Footer() {
         </div>
         <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
          
-          <p>© 2025 Company, Inc. All rights reserved.</p>
+          <p>© 2025 Company, Inc. All rights reserveda.</p>
           <ul className="list-unstyled d-flex">
            
             <li className="ms-3">
