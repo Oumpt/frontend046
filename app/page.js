bf9f74619a5ee12145e1711b75840c6e429d6b20
Oumpt <pongthepuon@gmail.com>
2025-07-07 11:59:30 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+import Carousel from './component/carousel';
+import Card from './component/card';
 export default function Home() {
   return (
-    <>
-    <h1 className="ftext-4xl text-center text-blue-950">หน้าแรก</h1>
-    </>
+      <main >
+          <Carousel/> 
+          <Card/>
+      </main>
+      
+
   )
 }
+
+
