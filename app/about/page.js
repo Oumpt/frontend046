e73c3e1cc24205681a29c689ccfe0298e82cc98a
Oumpt <pongthepuon@gmail.com>
2025-07-07 10:23:07 +0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <>
-    <h1 className="ftext-4xl text-center text-blue-950">About Page</h1>
+    <h1 className="ftext-4xl text-center text-blue-950">เกี่ยวกับเรา</h1>
     </>
   )
 }
