9ea439f9e0a615427d971159a8c13ba174d5794c
Oumpt <pongthepuon@gmail.com>
2025-07-07 12:18:18 +0700

    Update navigation.js

--- app/component/navigation.js
+++ app/component/navigation.js
@@ -1,50 +1,45 @@
-// app/components/Navigation.tsx
-'use client';
-import Link from 'next/link';
+//import ใน /app/layout.js
 
-export default function Navigation() {
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
   return (
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-<a className="navbar-brand" href="/">
-  <img src="/bootstrap-logo.svg" alt="Logo" width={30} height={24} className="d-inline-block align-text-top" />   Frontend
-</a>
-
+  <Link href="/" className="navbar-brand d-flex align-items-center gap-2"><img src="/bootstrap-logo.svg" alt="Logo" width={30} height={24} className="d-inline-block align-text-top" /> FrontEnd</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">หน้าแรก</a>
+          <Link className="nav-link active" aria-current="page" href="/">หน้าแรก</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">เกี่ยวกับเรา</a>
+          <Link className="nav-link" href="/about">เกี่ยวกับเรา</Link>
         </li>
         <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/service" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link className="nav-link dropdown-toggle" href="/service" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             บริการของเรา
-          </a>
+          </Link>
           <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/service">ติดต่อ</a></li>
-            <li><a className="dropdown-item" href="/service">ติดต่อด้านอื่นๆ</a></li>
+            <li><Link className="dropdown-item" href="/service">ติดต่อเรา</Link></li>
+            <li><Link className="dropdown-item" href="/service">ติดต่อ ด้านอื่นๆ</Link></li>
             <li><hr className="dropdown-divider" /></li>
-            <li><a className="dropdown-item" href="/service">สอบถามเกี่ยวกับปัญหา</a></li>
+            <li><Link className="dropdown-item" href="/service">สอบถามเกียวกับปัญหา</Link></li>
           </ul>
         </li>
         <li className="nav-item">
-          <a className="nav-link " aria-disabled="true" href="/contact">ติดต่อเรา</a>
+          <Link href="/contact" className="nav-link" aria-disabled="true">ติดต่อเรา</Link>
         </li>
       </ul>
       <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">ค้นหา</button>
+        <input className="form-control me-2" type="search" placeholder="ค้นหา" aria-label="Search" />
+        <button className="btn btn-outline-warning" type="submit">ค้นหา</button>
       </form>
     </div>
   </div>
 </nav>
-
-    
-  
   );
 }
