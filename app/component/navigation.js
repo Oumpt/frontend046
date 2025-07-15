b359b26ea933822644e3a0e6aae235e6cb606070
Oumpt <pongthepuon@gmail.com>
2025-07-15 16:12:25 +0700

    Update navigation.js

--- app/component/navigation.js
+++ app/component/navigation.js
@@ -6,24 +6,24 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-<nav className="navbar navbar-expand-lg fixed-top bg-body-tertiary navbar-black" style={{
+<nav className="navbar navbar-expand-lg fixed-top " style={{ backdropFilter:"blur(13px)" , backgroundColor: "rgb(0,0,0,0)"
 
     }}>
   <div className="container-fluid">
-  <Link href="/" className="navbar-brand d-flex align-items-center gap-2"><img src="/bootstrap-logo.svg" alt="Logo" width={30} height={24} className="d-inline-block align-text-top" /> FrontEnd</Link>
+  <Link href="/" className="navbar-brand d-flex align-items-center gap-2" style={{color:"white"}}><img src="/bootstrap-logo.svg" alt="Logo" width={30} height={24} className="d-inline-block align-text-top" /> FrontEnd</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" href="/">หน้าแรก</Link>
+          <Link className="nav-link active" aria-current="page" href="/" style={{color:"white"}}>หน้าแรก</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" href="/about">เกี่ยวกับเรา</Link>
+          <Link className="nav-link" href="/about" style={{color:"white"}}>เกี่ยวกับเรา</Link>
         </li>
         <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" href="/service" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link className="nav-link dropdown-toggle" href="/service" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{color:"white"}}>
             บริการของเรา
           </Link>
           <ul className="dropdown-menu">
@@ -34,12 +34,12 @@ export default function Navbar() {
           </ul>
         </li>
         <li className="nav-item">
-          <Link href="/contact" className="nav-link" aria-disabled="true">ติดต่อเรา</Link>
+          <Link href="/contact" className="nav-link" aria-disabled="true" style={{color:"white"}}>ติดต่อเรา</Link>
         </li>
       </ul>
 
       <div className="ms-2">    
-          <a href="\login" className="btn btn-primary " tabIndex={-1} role="button" >เข้าสู่ระบบ</a></div>
+          <a href="\login" className="btn btn-primary " tabIndex={-1} role="button" style={{color:"white"}}>เข้าสู่ระบบ</a></div>
 
     </div>
   </div>
