eb36e50923d3d7a5c75667af92d53d7748c4b62c
Oumpt <pongthepuon@gmail.com>
2025-07-21 09:36:32 +0700

    Update footer.js

--- app/component/footer.js
+++ app/component/footer.js
@@ -1,147 +1,85 @@
-//import ใน /app/layout.js
+'use client'
 
 export default function Footer() {
   return (
-    <div className="container-fluid">
-      <footer className="py-5">
-        <div className="row">
+    <div className="container-fluid mt-5">
+      <footer
+        className="py-5"
+        style={{
+          background: 'rgba(255, 255, 255, 0.1)',
+          backdropFilter: 'blur(10px)',
+          borderRadius: '16px',
+          border: '1px solid rgba(255,255,255,0.2)',
+          color: '#fff',
+          boxShadow: '0 8px 24px rgba(0, 0, 0, 0.2)',
+        }}
+      >
+        <div className="row px-4">
           <div className="col-6 col-md-2 mb-3">
             <h5>Section</h5>
             <ul className="nav flex-column">
-       
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Home
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Home</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Features
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Features</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Pricing
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Pricing</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  FAQs
-                </a>
+                <a href="#" className="nav-link p-0 text-white">FAQs</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  About
-                </a>
+                <a href="#" className="nav-link p-0 text-white">About</a>
               </li>
             </ul>
           </div>
+
           <div className="col-6 col-md-2 mb-3">
-           
-            <h5>Section</h5>
+            <h5>More</h5>
             <ul className="nav flex-column">
-             
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Home
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Blog</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Features
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Careers</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Pricing
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Support</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  About
-                </a>
+                <a href="#" className="nav-link p-0 text-white">Privacy</a>
               </li>
             </ul>
           </div>
-          <div className="col-6 col-md-2 mb-3">
-           
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-             
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-body-secondary">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
+
           <div className="col-md-5 offset-md-1 mb-3">
-           
             <form>
-             
               <h5>Subscribe to our newsletter</h5>
               <p>Monthly digest of what's new and exciting from us.</p>
               <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-               
-                <label htmlFor="newsletter1" className="visually-hidden">
-                  Email address
-                </label>
+                <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
                 <input
                   id="newsletter1"
                   type="email"
                   className="form-control"
                   placeholder="Email address"
                 />
-                <button className="btn btn-primary" type="button">
-                  Subscribe
-                </button>
+                <button className="btn btn-outline-light" type="button">Subscribe</button>
               </div>
             </form>
           </div>
         </div>
-        <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-         
-          <p>© 2025 Company, Inc. All rights reserved.</p>
+
+        <div className="d-flex flex-column flex-sm-row justify-content-between py-4 px-4 my-4 border-top border-light">
+          <p className="mb-0">© 2025 Company, Inc. All rights reserved.</p>
           <ul className="list-unstyled d-flex">
-           
             <li className="ms-3">
-              <a className="link-body-emphasis" href="#" aria-label="Instagram">
-                <svg className="bi" width={24} height={24}>
-                  <use xlinkHref="#instagram" />
-                </svg>
-              </a>
+              <a className="text-white" href="#"><i className="bi bi-instagram fs-5"></i></a>
             </li>
             <li className="ms-3">
-              <a className="link-body-emphasis" href="#" aria-label="Facebook">
-                <svg className="bi" width={24} height={24} aria-hidden="true">
-                  <use xlinkHref="#facebook" />
-                </svg>
-              </a>
+              <a className="text-white" href="#"><i className="bi bi-facebook fs-5"></i></a>
             </li>
           </ul>
         </div>
