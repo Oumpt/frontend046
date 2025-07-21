f7acf6dd232f18ef2f8daaa7cc1577fc96fd5933
Oumpt <pongthepuon@gmail.com>
2025-07-21 12:25:59 +0700

    Update page.js

--- app/service/page.js
+++ app/service/page.js
@@ -1,7 +1,7 @@
 export default function Service() {
   return (
     <div>
-      <h1 className="ftext-4lx text-center" style={{ height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px' }}>บริการของเรา</h1>
+      <h1 className="ftext-4lx text-center text-light" style={{ height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px' }}>บริการของเรา</h1>
     </div>
   )
 }
