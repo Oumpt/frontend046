fc085b33528df4b5027a04bf41133131561cd228
Oumpt <pongthepuon@gmail.com>
2025-07-21 09:51:36 +0700

    Update footer.js

--- app/component/footer.js
+++ app/component/footer.js
@@ -15,49 +15,31 @@ export default function Footer() {
         }}
       >
         <div className="row px-4">
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
+          <div className="col-6 col-md-3 mb-3">
+            <h5>เมนูหลัก</h5>
             <ul className="nav flex-column">
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Home</a>
+                <a href="/" className="nav-link p-0 text-white">หน้าแรก</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Features</a>
+                <a href="/about" className="nav-link p-0 text-white">เกี่ยวกับเรา</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Pricing</a>
+                <a href="/service" className="nav-link p-0 text-white">บริการระบบ</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">FAQs</a>
+                <a href="/contact" className="nav-link p-0 text-white">ติดต่อเรา</a>
               </li>
               <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">About</a>
+                <a href="/login" className="nav-link p-0 text-white">เข้าสู่ระบบ</a>
               </li>
             </ul>
           </div>
 
-          <div className="col-6 col-md-2 mb-3">
-            <h5>More</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Blog</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Careers</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Support</a>
-              </li>
-              <li className="nav-item mb-2">
-                <a href="#" className="nav-link p-0 text-white">Privacy</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-md-5 offset-md-1 mb-3">
+          <div className="col-md-6 offset-md-1 mb-3">
             <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
+              <h5>ติดตามข่าวสาร</h5>
+              <p>สมัครรับข่าวสารและอัปเดตจากเราได้ทุกเดือน</p>
               <div className="d-flex flex-column flex-sm-row w-100 gap-2">
                 <label htmlFor="newsletter1" className="visually-hidden">Email address</label>
                 <input
