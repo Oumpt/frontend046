2385d0641374e4c35233532f44ebe22455a7b74d
Oumpt <pongthepuon@gmail.com>
2025-07-21 12:25:49 +0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div>
-      <h1 className="ftext-4lx text-center" style={{ height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px' }}>เกี่ยวกับเรา</h1>
+      <h1 className="ftext-4lx text-center text-light" style={{ height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px' }}>เกี่ยวกับเรา</h1>
     </div>
   )
 }
