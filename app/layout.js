04214e595d240f22d7c299a274ee5402f63418f9
Oumpt <pongthepuon@gmail.com>
2025-07-21 11:14:54 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -3,53 +3,47 @@ import Navigation from "./component/navigation";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Footer from './component/footer';
-
+import BackgroundMusic from "./component/backgroundmusic";
 
 import { Prompt } from 'next/font/google';
 const prompt = Prompt({
-  subsets: ['thai', 'latin'], // รองรับภาษาไทย
+  subsets: ['thai', 'latin'],
   weight: ['300', '400', '500', '600', '700'],
   display: 'swap',
 });
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
       <head>
         <title>FrontEnd046 Pongthep</title>
-        <meta name="viewport saturate-200" content="device-width,"/>  
-                      <style jsx global>{`
-        /* ซ่อน scrollbar สำหรับ Webkit browsers (Chrome, Safari, Edge) */
-        ::-webkit-scrollbar {
-          display: none;
-        }
-        
-        /* ซ่อน scrollbar สำหรับ Firefox */
-        html {
-          scrollbar-width: none;
-        }
-        
-        /* ซ่อน scrollbar แต่ยังสามารถเลื่อนได้ */
-        body {
-          -ms-overflow-style: none;  /* Internet Explorer 10+ */
-          scrollbar-width: none;  /* Firefox */
-        }
-        
-        body::-webkit-scrollbar { 
-          display: none;  /* Safari and Chrome */
-        }
-      `}</style>  
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style jsx global>{`
+          /* ซ่อน scrollbar สำหรับ Webkit browsers (Chrome, Safari, Edge) */
+          ::-webkit-scrollbar {
+            display: none;
+          }
+          html {
+            scrollbar-width: none;
+          }
+          body {
+            -ms-overflow-style: none;
+            scrollbar-width: none;
+          }
+          body::-webkit-scrollbar {
+            display: none;
+          }
+        `}</style>
       </head>
-      <body className={prompt.className} style={{backgroundColor: "gray"}}>
-
+      <body className={prompt.className} style={{ backgroundColor: 'gray' }}>
         <Navigation />
 
-          {children}
+        {children}
 
-      
+        <Footer />
 
-       <Footer />
+        {/* วางปุ่มเพลงไว้ข้างล่างสุด */}
+        <BackgroundMusic />
       </body>
     </html>
   );
