489ace0ee879c35fe43725080bb5a3d5b525023d
Oumpt <pongthepuon@gmail.com>
2025-07-21 08:21:22 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-
+'use client'
 import Navigation from "./component/navigation";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -13,22 +13,40 @@ const prompt = Prompt({
 });
 
 
-
-export const metadata = {
-  title: "Frontend046 Pongthep",
-  description: "Frontend046 Pongthep",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="th">
+      <head>
+        <title>FrontEnd046 Pongthep</title>
+        <meta name="viewport saturate-200" content="device-width,"/>
+              <style jsx global>{`
+        /* ซ่อน scrollbar สำหรับ Webkit browsers (Chrome, Safari, Edge) */
+        ::-webkit-scrollbar {
+          display: none;
+        }
+        
+        /* ซ่อน scrollbar สำหรับ Firefox */
+        html {
+          scrollbar-width: none;
+        }
+        
+        /* ซ่อน scrollbar แต่ยังสามารถเลื่อนได้ */
+        body {
+          -ms-overflow-style: none;  /* Internet Explorer 10+ */
+          scrollbar-width: none;  /* Firefox */
+        }
+        
+        body::-webkit-scrollbar { 
+          display: none;  /* Safari and Chrome */
+        }
+      `}</style>
+      </head>
       <body className={prompt.className} style={{backgroundColor: "gray"}}>
 
         <Navigation />
-        <main style={{ paddingTop: '55px' }}>  
+
           {children}
 
-        </main>
       
 
        <Footer />
