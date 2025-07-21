6e87a7922c2dc1d54aff0ae9981773f216a9ee3b
Oumpt <pongthepuon@gmail.com>
2025-07-21 12:25:53 +0700

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,7 @@
 export default function Contact() {
   return (
     <div>
-      <h1 className="ftext-4lx text-center" style={{ height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px' }}>ติดต่อเรา</h1>
+      <h1 className="ftext-4lx text-center text-light" style={{ height: '100vh', width: '100vw', padding: '20px', margin: '0 auto', marginTop: '100px' }}>ติดต่อเรา</h1>
     </div>
   )
 }
